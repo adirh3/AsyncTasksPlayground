d33ccf986006554c878ed8637226bb242f8451fb
Adir <adirh3@gmail.com>
2021-01-16 23:55:54 +0200

    Added really basic ValueTask sample

--- ValueTasks/Program.cs
+++ ValueTasks/Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace ValueTasks
 {
-    internal class Program
+    internal static class Program
     {
-        private static void Main()
+        private static async Task Main()
         {
-            
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            await MaybeRunTask(true);
+            Console.WriteLine($"Task with true: {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+            await MaybeRunValueTask(true);
+            Console.WriteLine($"ValueTask with true: {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+            await MaybeRunTask(false);
+            Console.WriteLine($"Task with false: {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+            await MaybeRunValueTask(false);
+            Console.WriteLine($"ValueTask with false: {stopwatch.ElapsedMilliseconds}");
         }
 
-        private static async Task<int> MaybeRunAsync(bool shouldIRun)
+        private static async Task<int> MaybeRunTask(bool shouldIRun)
+        {
+            if (shouldIRun)
+                return 0;
+
+            await Task.Delay(100);
+            return 5;
+        }
+        
+        private static async ValueTask<int> MaybeRunValueTask(bool shouldIRun)
         {
             if (shouldIRun)
                 return 0;
